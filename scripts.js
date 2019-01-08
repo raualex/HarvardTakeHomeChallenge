4bf24c751021367baf47d5e5b1d2844ba5e50b98
Alexander Rau <alexanderrau86@yahoo.com>
2019-01-08 15:26:21 -0700

    Build next page button functionality

--- scripts.js
+++ scripts.js
@@ -1,21 +1,47 @@
 Vue.component('art-image', {
-  props: ['image'],
-  template: `<img src={image.baseimageurl} alt='Image from the Harvard Art Museum' />`
+  template: `<img src={image.baseimageurl} alt='Image from the Harvard Art Museum' class='art-image' />`
+})
+
+Vue.component('main-title', {
+  template: `<h1>Harvard Museum of Art</h1>`
 })
 
 var app = new Vue({
   el: '#app',
   data: {
-    imageUrls: null
+      page: 1,
+      imageUrls: null
+  },
+  methods: {
+    changePage: function() {
+      if (this.page >= 1) {
+        this.page = this.page += 1
+      } else {
+        return
+      }
+    }
   },
   mounted: function() {
     let finalPics = []
-    fetch('https://api.harvardartmuseums.org/image?apikey=5c4be6d0-12cb-11e9-90d1-473127181d8c&page=2')
+    fetch(`https://api.harvardartmuseums.org/image?apikey=5c4be6d0-12cb-11e9-90d1-473127181d8c&page=${this.page}&q=height:800`)
       .then(response => response.json())
       .then(data => data.records.forEach((pic) => {
         finalPics.push( { fileid: pic.fileid, baseimageurl: pic.baseimageurl } )
       }))
       console.log(finalPics)
     this.imageUrls = finalPics
+  },
+  watch: {
+    page: function (num) {
+      console.log(num)
+      let finalPics = []
+      fetch(`https://api.harvardartmuseums.org/image?apikey=5c4be6d0-12cb-11e9-90d1-473127181d8c&page=${num}&q=height:800`)
+        .then(response => response.json())
+        .then(data => data.records.forEach((pic) => {
+          finalPics.push( { fileid: pic.fileid, baseimageurl: pic.baseimageurl } )
+        }))
+      console.log(finalPics)
+    this.imageUrls = finalPics
+    }
   }
 });
