4f04e602e30127c9118712105cb96e1f1b888eec
Alexander Rau <alexanderrau86@yahoo.com>
2019-01-08 18:28:53 -0700

    Build out most of the final styles

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 Vue.component('art-image', {
-  template: `<img src={image.baseimageurl} alt='Image from the Harvard Art Museum' class='art-image' />`
+  props: { image: Object },
+  template: `<div class='image-frame'><img :src='image.baseimageurl' alt='Image from the Harvard Art Museum' class='art-image' /></div>`
 })
 
 Vue.component('main-title', {
@@ -29,10 +30,8 @@ var app = new Vue({
     }
   },
   mounted: function() {
-    // import key from './utils/API/APIkey.js'
     let finalPics = []
-    // fetch(`https://api.harvardartmuseums.org/image?apikey=${key}&page=${this.page}&q=height:800`)
-    fetch(`https://api.harvardartmuseums.org/image?apikey=5c4be6d0-12cb-11e9-90d1-473127181d8c&page=${this.page}&q=height:800`)
+    fetch(`https://api.harvardartmuseums.org/image?apikey=${key}&page=${this.page}&q=height:800`)
       .then(response => response.json())
       .then(data => data.records.forEach((pic) => {
         finalPics.push( { fileid: pic.fileid, baseimageurl: pic.baseimageurl } )
@@ -42,10 +41,8 @@ var app = new Vue({
   },
   watch: {
     page: function (num) {
-      // import key from './utils/API/APIkey.js'
       let finalPics = []
-      // fetch(`https://api.harvardartmuseums.org/image?apikey=${key}&page=${num}&q=height:800`)
-      fetch(`https://api.harvardartmuseums.org/image?apikey=5c4be6d0-12cb-11e9-90d1-473127181d8c&page=${num}&q=height:800`)
+      fetch(`https://api.harvardartmuseums.org/image?apikey=${key}&page=${num}&q=height:800`)
         .then(response => response.json())
         .then(data => data.records.forEach((pic) => {
           finalPics.push( { fileid: pic.fileid, baseimageurl: pic.baseimageurl } )
