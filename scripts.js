1abe1a327f7eb3120b7baf3286097aebf8c2684c
Alexander Rau <alexanderrau86@yahoo.com>
2019-01-08 17:05:57 -0700

    Add more features/styles

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ Vue.component('art-image', {
 })
 
 Vue.component('main-title', {
-  template: `<h1>Harvard Museum of Art</h1>`
+  template: `<h1 class='main-title'>Harvard Museum of Art</h1>`
 })
 
 var app = new Vue({
@@ -22,8 +22,9 @@ var app = new Vue({
     }
   },
   mounted: function() {
+    import key from './utils/API/APIkey.js'
     let finalPics = []
-    fetch(`https://api.harvardartmuseums.org/image?apikey=5c4be6d0-12cb-11e9-90d1-473127181d8c&page=${this.page}&q=height:800`)
+    fetch(`https://api.harvardartmuseums.org/image?apikey=${key}&page=${this.page}&q=height:800`)
       .then(response => response.json())
       .then(data => data.records.forEach((pic) => {
         finalPics.push( { fileid: pic.fileid, baseimageurl: pic.baseimageurl } )
@@ -33,9 +34,9 @@ var app = new Vue({
   },
   watch: {
     page: function (num) {
-      console.log(num)
+      import key from './utils/API/APIkey.js'
       let finalPics = []
-      fetch(`https://api.harvardartmuseums.org/image?apikey=5c4be6d0-12cb-11e9-90d1-473127181d8c&page=${num}&q=height:800`)
+      fetch(`https://api.harvardartmuseums.org/image?apikey=${key}&page=${num}&q=height:800`)
         .then(response => response.json())
         .then(data => data.records.forEach((pic) => {
           finalPics.push( { fileid: pic.fileid, baseimageurl: pic.baseimageurl } )
