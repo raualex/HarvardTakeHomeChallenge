1f915a6c496a1632a6e6f152deb7d506c29b6e15
Alexander Rau <alexanderrau86@yahoo.com>
2019-01-08 17:23:04 -0700

    Implement Last Page button functionality

--- scripts.js
+++ scripts.js
@@ -13,18 +13,26 @@ var app = new Vue({
       imageUrls: null
   },
   methods: {
-    changePage: function() {
+    increasePage: function() {
       if (this.page >= 1) {
         this.page = this.page += 1
+      } else {
+        return
+      }
+    },
+    reducePage: function() {
+      if (this.page > 1) {
+        this.page = this.page -= 1
       } else {
         return
       }
     }
   },
   mounted: function() {
-    import key from './utils/API/APIkey.js'
+    // import key from './utils/API/APIkey.js'
     let finalPics = []
-    fetch(`https://api.harvardartmuseums.org/image?apikey=${key}&page=${this.page}&q=height:800`)
+    // fetch(`https://api.harvardartmuseums.org/image?apikey=${key}&page=${this.page}&q=height:800`)
+    fetch(`https://api.harvardartmuseums.org/image?apikey=5c4be6d0-12cb-11e9-90d1-473127181d8c&page=${this.page}&q=height:800`)
       .then(response => response.json())
       .then(data => data.records.forEach((pic) => {
         finalPics.push( { fileid: pic.fileid, baseimageurl: pic.baseimageurl } )
@@ -34,9 +42,10 @@ var app = new Vue({
   },
   watch: {
     page: function (num) {
-      import key from './utils/API/APIkey.js'
+      // import key from './utils/API/APIkey.js'
       let finalPics = []
-      fetch(`https://api.harvardartmuseums.org/image?apikey=${key}&page=${num}&q=height:800`)
+      // fetch(`https://api.harvardartmuseums.org/image?apikey=${key}&page=${num}&q=height:800`)
+      fetch(`https://api.harvardartmuseums.org/image?apikey=5c4be6d0-12cb-11e9-90d1-473127181d8c&page=${num}&q=height:800`)
         .then(response => response.json())
         .then(data => data.records.forEach((pic) => {
           finalPics.push( { fileid: pic.fileid, baseimageurl: pic.baseimageurl } )
